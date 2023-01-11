9723d168b207c8065eabf1819833b87bfc3d007e
BlackHatMan <38476233+BlackHatMan@users.noreply.github.com>
2023-01-11 16:21:18 +0300

    init scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/app.css';
-import '../styles/globals.css';
+import '../styles/app.scss';
+import '../styles/globals.scss';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps}></Component>;
 }
