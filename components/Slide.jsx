5b6c58e80a15a5d75fa162f2fd80cd0e4cdf3854
BlackHatMan <38476233+BlackHatMan@users.noreply.github.com>
2023-01-19 02:36:26 +0300

    feat: slider

---
+++ components/Slide.jsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import panda from '../public/panda.jpg';
+import Triangle from '../public/triangle.svg';
+import Dollar from '../public/dollar.svg';
+import style from './slide.module.scss';
+
+export default function Slide() {
+  return (
+    <div className={style.slide}>
+      <Image src={panda} fill={true} alt="panda" />
+      <div className={style.slideContent}>
+        <p>Panda’s name is Bei Bei. He is 2 years old. Bei Bei is from China. He loves bamboos.</p>
+        <div className={style.wrapLinks}>
+          <Link href="/">
+            <label className={style.ring}>
+              <Triangle />
+            </label>
+            <label className={style.watch}>Watch online</label>
+          </Link>
+          <Link href="/">
+            <label className={style.ring}>
+              <Dollar />
+            </label>
+            <label className={style.watch}>Donate</label>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,16 @@
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import panda from '../public/panda.jpg';
-import Triangle from '../public/triangle.svg';
-import Dollar from '../public/dollar.svg';
 import style from '../styles/welcome.module.scss';
+import Slide from '../components/Slide';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import 'swiper/css';
+
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 
 export default function Home() {
-  const a = '▶';
   return (
     <div className="home-container">
       <Head>
@@ -16,32 +19,39 @@ export default function Home() {
       </Head>
 
       <main>
-        <section className={style.wrapWelcome}>
+        <section className={style.container}>
           <h1>
             Watch your favorite <br /> animal online
           </h1>
-          <div className={style.wrapSlider}>
-            <div className={style.slider}>
-              <figure>
-                <Image src={panda} fill={true} alt="panda" />
-                <figcaption>
-                  <p>Panda’s name is Bei Bei. He is 2 years old. Bei Bei is from China. He loves bamboos.</p>
-                  <div className={style.wrapLinks}>
-                    <Link href="/">
-                      <label className={style.ring}>
-                        <Triangle />
-                      </label>
-                      <label className={style.watch}>Watch online</label>
-                    </Link>
-                    <Link href="/">
-                      <label className={style.ring}>
-                        <Dollar />
-                      </label>
-                      <label className={style.watch}>Donate</label>
-                    </Link>
-                  </div>
-                </figcaption>
-              </figure>
+          <div className={style.slider}>
+            <div className={style.wrapper}>
+              {
+                <Swiper
+                  modules={[Navigation, Pagination, Scrollbar, A11y]}
+                  spaceBetween={20}
+                  slidesPerView={3}
+                  pagination={{ clickable: true }}
+                >
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <Slide />
+                  </SwiperSlide>
+                </Swiper>
+              }
             </div>
           </div>
         </section>
