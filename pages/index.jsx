84d96afb709201b94b974a44b9f655a656746236
BlackHatMan <38476233+BlackHatMan@users.noreply.github.com>
2023-01-19 15:50:05 +0300

    fix: style slider

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,12 @@ import Head from 'next/head';
 import style from '../styles/welcome.module.scss';
 import Slide from '../components/Slide';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Pagination } from 'swiper';
 
 import 'swiper/css';
 
 import 'swiper/scss/navigation';
 import 'swiper/scss/pagination';
-import 'swiper/scss/scrollbar';
 
 export default function Home() {
   return (
@@ -23,37 +22,35 @@ export default function Home() {
           <h1>
             Watch your favorite <br /> animal online
           </h1>
-          <div className={style.slider}>
-            <div className={style.wrapper}>
-              {
-                <Swiper
-                  modules={[Navigation, Pagination, Scrollbar, A11y]}
-                  spaceBetween={20}
-                  slidesPerView={3}
-                  pagination={{ clickable: true }}
-                >
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <Slide />
-                  </SwiperSlide>
-                </Swiper>
-              }
-            </div>
-          </div>
+          {
+            <Swiper
+              className={style.slider}
+              createElements
+              modules={[Pagination]}
+              spaceBetween={20}
+              slidesPerView={3}
+              pagination={{ clickable: true }}
+            >
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+              <SwiperSlide>
+                <Slide />
+              </SwiperSlide>
+            </Swiper>
+          }
         </section>
       </main>
 
